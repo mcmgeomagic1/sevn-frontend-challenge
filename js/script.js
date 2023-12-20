4b3e15aa72adfc97d8c2ba763019a5cacecded77
mcmgeomagic1 <mcmgeomagic@gmail.com>
2023-12-20 02:44:14 +0100

    designed the rodadas card

--- js/script.js
+++ js/script.js
@@ -1,21 +1,5 @@
-const Menu =(menuitems)=> {
-    var ulElement = document.createElement("ul");
-    ulElement.classList.add("menu")
 
-    menuitems.forEach(function(item)  {
-            var liElement = document.createElement("li")
-            liElement.classList.add("menu-items")
-            liElement.appendChild(document.createTextNode(item))
-            ulElement.appendChild(liElement)
-    })
 
-    return ulElement
-}
-
-var divApp = document.getElementById("app");
-
-var items = ["Home", "About", "Contact"]
-
-var menuComponent = Menu(items)
-
-divApp.appendChild(menuComponent)
+const runApi = fetch("https://sevn-pleno-esportes.deno.dev")
+.then((res)=> res.json())
+.then((data)=> console.log("yo", data))
